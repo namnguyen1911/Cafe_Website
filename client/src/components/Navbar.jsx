3450a1e40b80f178680cfaa7b718e2822ecdf359
Nam Nguyen <namnh11@gmail.com>
2025-10-28 13:54:03 -0700

    add changes to Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,6 @@
+import { NavLink } from 'react-router-dom'
 import React from 'react'
+import {assets} from '../assets/assets'
 
 const Navbar = () => {
     const [open, setOpen] = React.useState(false)
@@ -23,42 +25,32 @@ const Navbar = () => {
 
             <div className="hidden sm:flex items-center gap-8">
 
-                <a href="#">Home</a>
+                <NavLink to='/'>Home</NavLink>
 
-                <a href="#">About</a>
+                <NavLink to='/products'>All Product</NavLink>
 
-                <a href="#">Contact</a>
+                <NavLink to='/'>Contact</NavLink>
 
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
 
                     <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
 
-                    <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
-
-                        <path d="M10.836 10.615 15 14.695" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
-
-                        <path clip-rule="evenodd" d="M9.141 11.738c2.729-1.136 4.001-4.224 2.841-6.898S7.67.921 4.942 2.057C2.211 3.193.94 6.281 2.1 8.955s4.312 3.92 7.041 2.783" stroke="#7A7B7D" stroke-width="1.2" stroke-linecap="round" stroke-linejoin="round" />
-
-                    </svg>
+                    <img src={assets.search_icon} alt='search' className='w-4 h-4'/>
 
                 </div>
 
 
                 <div className="relative cursor-pointer">
 
-                    <svg width="18" height="18" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80'/>
 
-                        <path d="M.583.583h2.333l1.564 7.81a1.17 1.17 0 0 0 1.166.94h5.67a1.17 1.17 0 0 0 1.167-.94l.933-4.893H3.5m2.333 8.75a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0m6.417 0a.583.583 0 1 1-1.167 0 .583.583 0 0 1 1.167 0" stroke="#615fff" stroke-linecap="round" stroke-linejoin="round" />
-
-                    </svg>
-
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-indigo-500 w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-red-400 hover:bg-red-600 w-[18px] h-[18px] rounded-full">3</button>
 
                 </div>
 
 
-                <button className="cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full">
+                <button className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
 
                     Login
 
