22b27b381d2ecb51b42b59776ba8b21ffa255aee
Nam Nguyen <namnh11@gmail.com>
2025-11-03 13:15:19 -0800

    Add banner

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainBanner from '../components/MainBanner'
+
+const Home = () => {
+  return (
+    <div classname='mt-12'>
+        <MainBanner/>
+
+    </div>
+  )
+}
+
+export default Home
