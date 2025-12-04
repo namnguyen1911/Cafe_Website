1418692f0ac6f8cbeeb69a9b668b1821841ce386
Nam Nguyen <namnh11@gmail.com>
2025-12-04 02:57:33 -0800

    fix allowedOrigins

--- server/server.js
+++ server/server.js
@@ -21,7 +21,7 @@ await connectDB()
 await connectCloudinary()
 
 //Allow multiple origins
-const allowedOrigins = ['http://localhost:5173','https://coffee-roastery-website-frontend.vercel.app']
+const allowedOrigins = ['http://localhost:5173','https://sauluc-coffee-roastery.vercel.app']
 
 app.post('/stripe', express.raw({type: 'application/json'}), stripeWebhooks)
 
