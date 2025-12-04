083e01b19047f98eb4484f8c706df59dc2df57fa
Nam Nguyen <namnh11@gmail.com>
2025-12-04 13:35:45 -0800

    Show instock product, if not no product found in ProductCategory.jsx

--- client/src/pages/ProductCategory.jsx
+++ client/src/pages/ProductCategory.jsx
@@ -10,8 +10,8 @@ const ProductCategory = () => {
     const {category} = useParams()
 
     const searchCategory = categories.find((item) => item.path.toLowerCase() === category)
-    const filteredProducts = products.filter((product) => product.category.toLowerCase() === category
-    )
+    const filteredProducts = products.filter((product) => product.category.toLowerCase() === category)
+    const availableProducts = filteredProducts.filter((product) => product.inStock)
   return (
     <div className='mt-16'>
         {searchCategory && (
@@ -22,9 +22,9 @@ const ProductCategory = () => {
                 <div className='w-16 h-0.5 bg-primary rounded-l-full'></div>
             </div>
         )}
-        {filteredProducts.length > 0 ? (
+        {availableProducts.length > 0 ? (
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-3 md:gap-6 mt-6'>
-                {filteredProducts.map((product) => (
+                {availableProducts.map((product) => (
                     <ProductCard key={product._id} product={product}/>
                 ))}
             </div>
