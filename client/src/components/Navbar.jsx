2a39527579969eaa1ef5783414a7adbac9f73ba3
Nam Nguyen <namnh11@gmail.com>
2025-12-06 01:29:54 -0800

    fix mobile dropdown menu my orders

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -78,7 +78,7 @@ const Navbar = () => {
                     <div className='relative group'>
                         <img src={assets.profile_icon} className='w-10' alt="profile_icon"/>
                         <ul className='hidden group-hover:block absolute top-10 right-0 bg-white shadow border border-gray-230 py-2.5 w-30 rounded-md text-sm z-40'>
-                            <li onClick={() => navigate("my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
+                            <li onClick={() => navigate("/my-orders")} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>My Orders</li>
                             <li onClick={logout} className='p-1.5 pl-3 hover:bg-primary/10 cursor-pointer'>Logout</li>
                         </ul>
                     </div>
@@ -118,7 +118,7 @@ const Navbar = () => {
 
                     <NavLink to='/products' onClick={() => setOpen(false)}>All Products</NavLink>
 
-                    { user && <NavLink to='/products' onClick={() => setOpen(false)}>My Orders</NavLink>}
+                    { user && <NavLink to='/my-orders' onClick={() => {setOpen(false);}}>My Orders</NavLink>}
 
                     <NavLink to='/' onClick={() => setOpen(false)}>Contact</NavLink>
 
